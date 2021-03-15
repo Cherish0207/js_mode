f570623edfd91ee6cfd12d6f57fbd64ffc363ae6
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-15 09:42:39 +0800

    13.职责链模式:改进

--- 13.职责链模式/2.职责链模式.js
+++ 13.职责链模式/2.职责链模式.js
@@ -2,14 +2,14 @@ const order500 = function (orderType, pay, stock) {
   if (orderType === 1 && pay === true) {
     console.log("500 元定金预购, 得到 100 优惠券");
   } else {
-    order200(orderType, pay, stock); // 将请求传递给 200 元订单
+    return "nextSuccessor";
   }
 };
 const order200 = function (orderType, pay, stock) {
   if (orderType === 2 && pay === true) {
     console.log("200 元定金预购, 得到 50 优惠券");
   } else {
-    ordernormal(orderType, pay, stock); // 将请求传递给普通订单
+    return "nextSuccessor";
   }
 };
 const ordernormal = function (orderType, pay, stock) {
@@ -19,8 +19,28 @@ const ordernormal = function (orderType, pay, stock) {
     console.log("手机库存不足");
   }
 };
+class Chain {
+  constructor(fn) {
+    this.fn = fn;
+    this.successor = null;
+  }
+  setNextSuccessor(successor) {
+    return (this.successor = successor);
+  }
+  exec() {
+    const nxt = this.fn.call(this, ...arguments);
+    if (nxt === "nextSuccessor") {
+      return this.successor.exec.call(this.successor, ...arguments);
+    }
+  }
+}
+
+const chain500 = new Chain(order500);
+const chain200 = new Chain(order200);
+const chainnormal = new Chain(ordernormal);
+chain500.setNextSuccessor(chain200).setNextSuccessor(chainnormal);
 order500(1, true, 500); // 输出：500 元定金预购, 得到 100 优惠券
-// order500( 1, false, 500 ); // 输出：普通购买, 无优惠券
+// chain500.exec(1, false, 500); // 输出：普通购买, 无优惠券
 // order500( 2, true, 500 ); // 输出：200 元定金预购, 得到 500 优惠券
 // order500( 3, false, 500 ); // 输出：普通购买, 无优惠券
 // order500( 3, false, 0 ); // 输出：手机库存不足
