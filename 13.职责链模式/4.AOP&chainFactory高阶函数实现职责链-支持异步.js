67e5955081cb9f81476b23135da41e2e8eac6be1
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-15 10:15:37 +0800

    13.职责链模式:AOP/chainFactory高阶函数实现职责链（支持异步）

---
+++ 13.职责链模式/4.AOP&chainFactory高阶函数实现职责链-支持异步.js
@@ -0,0 +1,62 @@
+const order500 = function (orderType, pay, stock) {
+  if (orderType === 1 && pay === true) {
+    console.log("500 元定金预购, 得到 100 优惠券");
+  } else {
+    return "nextSuccessor";
+  }
+};
+const order200 = function (orderType, pay, stock) {
+  if (orderType === 2 && pay === true) {
+    console.log("200 元定金预购, 得到 50 优惠券");
+  } else {
+    return "nextSuccessor";
+  }
+};
+const ordernormal = function (orderType, pay, stock) {
+  console.log(orderType, pay, stock);
+
+  if (stock > 0) {
+    console.log("普通购买, 无优惠券");
+  } else {
+    console.log("手机库存不足");
+  }
+};
+const chainFactory = (fn) => {
+  return {
+    fn,
+    successor: null,
+    setNextSuccessor(successor) {
+      return (this.successor = successor);
+    },
+    exec() {
+      const nxt = this.fn.call(this, ...arguments);
+      if (nxt === "nextSuccessor") {
+        return (
+          this.successor &&
+          this.successor.exec.call(this.successor, ...arguments)
+        );
+      }
+    },
+    next() {
+      return (
+        this.successor && this.successor.exec.call(this.successor, ...arguments)
+      );
+    },
+  };
+};
+const fn1 = chainFactory(function () {
+  console.log(1);
+  return "nextSuccessor";
+});
+const fn2 = chainFactory(function () {
+  console.log(2);
+  var self = this;
+  setTimeout(function () {
+    self.next();
+  }, 1000);
+});
+const fn3 = chainFactory(function () {
+  console.log(3);
+});
+fn1.setNextSuccessor(fn2).setNextSuccessor(fn3);
+fn1.exec();
